2d8731cf342529b68894fb754240196823779524
GuilhermeSergio <gui.sergiiio@gmail.com>
2022-11-08 18:41:38 -0300

    Modificação program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,7 +13,18 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Reserva Gui = new  Reserva(101, LocalDate.parse("2022-11-08"), LocalDate.parse("2022-11-10"));
+		System.out.print("Numero do quarto: ");
+		int numeroQuarto = sc.nextInt();
+		
+		DateTimeFormatter fm1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.print("Check-in: ");
+		LocalDate checkin = LocalDate.parse(sc.next(),fm1);
+		System.out.print("Check-out: ");
+		LocalDate checkout = LocalDate.parse(sc.next(),fm1);
+	
+		Reserva Gui = new  Reserva(numeroQuarto, checkin, checkout);
+		
 		System.out.print(Gui.toString());
 		
 		sc.close();
